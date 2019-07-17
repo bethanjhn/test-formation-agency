623c1e3c2e914584aae90df646435ee165f58ee8
bethanjhn <46195797+bethanjhn@users.noreply.github.com>
2019-07-17 12:57:42 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,8 +9,9 @@ html = scraperwiki.scrape("https://beta.companieshouse.gov.uk/company/04503188")
 #
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-root.cssselect("title[data-id='company_profile']")
-print html
+name = root.cssselect("title[data-id='company_profile']")
+
+print name
 #
 # # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
