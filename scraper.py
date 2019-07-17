1e9161711c625a835ab3808413f846e123044ddf
bethanjhn <46195797+bethanjhn@users.noreply.github.com>
2019-07-17 13:29:30 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ for companyname in name:
     print companyname.text
 
 # Find a second thing
-address = root.cssselect('dd.text-data')
+address = root.cssselect("dd.text-data company-status")
 
 for companyaddress in address:
     print companyaddress.text
